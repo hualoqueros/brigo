33ba7bbb5686f58e33485d8f735323937067ed6e
samudraarenza17 <samudra@clappingape.com>
2021-01-09 17:57:58 +0700

    generate signature

--- brigo.go
+++ brigo.go
@@ -1,7 +1,11 @@
 package brigo
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -16,7 +20,16 @@ type BRIConfig struct {
 }
 
 type BRICredentials struct {
-	Token string `json:"access_token"`
+	Token  string    `json:"access_token"`
+	Config BRIConfig `json:"config"`
+}
+
+type Payload struct {
+	Path      string `json:"path"`
+	Verb      string `json:"verb"`
+	Token     string `json:"token"`
+	Timestamp string `json:"timestamp"`
+	Body      string `json:"body`
 }
 
 func InitBRI(config BRIConfig) (briCred *BRICredentials, err error) {
@@ -50,6 +63,30 @@ func InitBRI(config BRIConfig) (briCred *BRICredentials, err error) {
 	}
 
 	return &BRICredentials{
-		Token: briCred.Token,
+		Token:  briCred.Token,
+		Config: config,
 	}, nil
+}
+
+func (bg *BRICredentials) CreateSignature(payload Payload) (signature string, err error) {
+
+	secret := bg.Config.ConsumerSecret
+	data := fmt.Sprintf(`path=%s&verb=%s&token=%s&timestamp=%s&body=%s`,
+		payload.Path,
+		payload.Verb,
+		payload.Token,
+		payload.Timestamp,
+		payload.Body)
+
+	// Get result and encode as hexadecimal string
+	signature = ComputeHmac256(data, secret)
+
+	return signature, err
+}
+
+func ComputeHmac256(message string, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
--- brigo_test.go
+++ brigo_test.go
@@ -1,6 +1,7 @@
 package brigo
 
 import (
+	"encoding/json"
 	"testing"
 )
 
@@ -18,5 +19,35 @@ func TestGetToken(t *testing.T) {
 	}
 
 	t.Logf("BERHASIL : %+v", briInit)
+
+}
+
+func TestSignature(t *testing.T) {
+
+	briConfig := BRIConfig{
+		ConsumerKey:    "E4Ar8prAnXGKO7S6lPTqJWVcOKZqzN1G",
+		ConsumerSecret: "M8YgS30WASAkbaZU",
+	}
+
+	bri, err := InitBRI(briConfig)
+	timestamp := "2019-01-02T13:14:15.678Z"
+	bodyStruct := map[string]interface{}{
+		"hello": "world",
+	}
+	body, _ := json.Marshal(bodyStruct)
+
+	payload := Payload{
+		Path:      "/v1/transfer/internal",
+		Verb:      "POST",
+		Token:     "Bearer " + bri.Token,
+		Timestamp: timestamp,
+		Body:      string(body),
+	}
+	signature, err := bri.CreateSignature(payload)
+	if err != nil {
+		t.Errorf("SALAH! = %+v", err)
+	}
+
+	t.Logf("BERHASIL : %+v", signature)
 
 }
